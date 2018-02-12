7225f0966559ec6aa0210f525402c7cca42d7d9b
michaelroudnitski <michaelroudnitski@yahoo.ca>
2018-02-12 16:30:26 -0500

    added room model and formatting to admin page

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Room
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['property_name', 'address', 'host_name', 'cost']
--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    # add roommate preferences in the future
+    host_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    property_name = models.CharField(max_length=30, default="")
+    description = models.TextField(default="")
+    cost = models.DecimalField(max_digits=7, decimal_places=2)
+    number_of_rooms = models.PositiveSmallIntegerField()
+    last_updated = models.DateTimeField('last updated')
+
+    def __str__(self):
+        return self.property_name
