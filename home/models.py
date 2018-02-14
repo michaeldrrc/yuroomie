75675efbf39bd528562c3fb75e6a339215bf3cfc
michaelroudnitski <michaelroudnitski@yahoo.ca>
2018-02-13 22:11:26 -0500

    Added cards for latest listings on the home page

--- home/models.py
+++ home/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 class Room(models.Model):
+    """model for each room listing"""
     # add roommate preferences in the future
     host_name = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
     property_name = models.CharField(max_length=30, default="")
-    description = models.TextField(default="")
-    cost = models.DecimalField(max_digits=7, decimal_places=2) # change to PositiveIntegerField
+    description = models.TextField(default="No Description")
+    cost = models.PositiveIntegerField(default=0)
+    visists = models.PositiveIntegerField(default=0)
     number_of_rooms = models.PositiveSmallIntegerField()
     last_updated = models.DateTimeField('last updated')
 
--- home/views.py
+++ home/views.py
@@ -4,13 +4,12 @@ from django.http import Http404
 from .models import Room
 
 def index(request):
-    latest_posts_list = Room.objects.order_by('-last_updated')[:5]
+    latest_posts_list = Room.objects.order_by('-last_updated')[:6]
     return render(request, 'home/index.html', {'latest_posts_list': latest_posts_list})
 
 def detail(request, room_id):
     try:
         room = Room.objects.get(pk=room_id)
     except Room.DoesNotExist:
-        raise Http404("It's not you, it's us. We couldn't find the listing you were looking for")
-
+        raise Http404("OOPS. We couldn't find the listing you were looking for")
     return render(request, 'home/detail.html', {'room': room})
