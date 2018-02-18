0b7e38eca936604f801bb17bf5a75c0b7c7569b4
michaelroudnitski <michaelroudnitski@gmail.com>
2018-02-18 16:25:41 -0500

    added a browse all page with pagination

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.index, name='index'),
+    path('browse/', views.browse_rooms, name='browse'),
     path('r/<int:room_id>/', views.detail, name='detail'),
     path('r/create', views.create, name='create')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import RoomPostForm
-
 from .models import Room, RoomImage
 
 def index(request):
     latest_posts_list = Room.objects.order_by('-last_updated')[:4]
     return render(request, 'home/index.html', {'latest_posts_list': latest_posts_list,})
 
+def browse_rooms(request):
+    rooms_list = Room.objects.all()
+    paginator = Paginator(rooms_list, 2)
+    page = request.GET.get('page')
+    try:
+        rooms = paginator.page(page)
+    except PageNotAnInteger:
+        rooms = paginator.page(1)
+    except EmptyPage:
+        pasts = paginator.page(paginator.num_pages)
+    return render(request, 'home/browse.html', {'page': page,
+                                                'rooms': rooms})
+
 def detail(request, room_id):
     room = get_object_or_404(Room, pk=room_id)
     roomImages = list(room.roomimage_set.all())
     return render(request, 'home/detail.html', {'room': room, 'roomImages': roomImages})
-    
+
 def create(request):
     if request.method == 'POST':
         print(request.POST['address']) # we can pull and manipulate each attribute given in the POST using 'request.POST['name_of_field_in_html']'
