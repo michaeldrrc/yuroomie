ab093455078843d60dd539bb50c158b6c8ac7eaa
michaelroudnitski <michaelroudnitski@yahoo.ca>
2018-02-21 21:50:14 -0500

    tweaks

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('edit-profile/', views.edit_profile, name='edit_profile'),
     path('signout/', views.signout, name='signout'),
-    path('profile/', views.profile, name='profile'),
     path('your-listings/', views.user_listings, name='user_listings'),
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', auth_views.PasswordResetCompleteView .as_view(), name='password_change_done'),
